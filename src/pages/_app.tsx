dac2c027e1e03ef1dbf75378cf773465f71dbae2
behlole <behloleaqil@gmail.com>
2023-05-15 22:14:57 -0400

    implemented tanstack query provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
 import {AppProps} from "next/app";
 import React from "react";
 import {useStored} from "@/store/useStored";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: false
+        }
+    }
+})
 
 function Jsonier({Component, pageProps}: AppProps) {
     const [isReady, setReady] = React.useState(false);
     const isLightMode = useStored(state => state.lightMode);
+
+    React.useEffect(() => {
+        setReady(true);
+    }, [])
+    if (isReady) {
+        <QueryClientProvider client={queryClient}>
+
+        </QueryClientProvider>
+    }
 }
