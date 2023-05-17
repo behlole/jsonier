fb246669692b01ac1a6307df0db51ff0ca6aab79
behlole <behloleaqil@gmail.com>
2023-05-16 21:18:11 -0400

    implemented ModalController

--- src/containers/ModalController/index.tsx
+++ src/containers/ModalController/index.tsx
@@ -1,3 +1,35 @@
+import useModal from "../../store/useModal";
+import {shallow} from "zustand/shallow";
+
 export const ModalController = () => {
+    const setVisible = useModal(state => state.setVisible);
+    const [
+        importModal,
+        clearModal,
+        downloadModal,
+        settingsModal,
+        cloudModal,
+        accountModal,
+        shareModal,
+        loginModal,
+        premiumModal
+    ] = useModal(
+        state => [
+            state.import,
+            state.clear,
+            state.download,
+            state.settings,
+            state.cloud,
+            state.account,
+            state.share,
+            state.login,
+            state.premium
+        ],
+        shallow
+    )
+    return (
+        <>
+        </>
+    )
 
 }
