381a1ece6415e3e0b31349992befd411f15a206d
behlole <behloleaqil@gmail.com>
2023-05-16 21:09:41 -0400

    fixed useUser.tsx file bug

--- src/store/useUser.tsx
+++ src/store/useUser.tsx
@@ -1,8 +1,8 @@
-import {AltogicAuth, User} from "../typings/altogic";
-import {create} from "zustand";
-import altogic from "../api/altogic";
 import toast from "react-hot-toast";
+import {create} from "zustand";
+import {AltogicAuth, User} from "src/typings/altogic";
 import useModal from "./useModal";
+import altogic from "../api/altogic";
 
 interface UserActions {
     login: (response: AltogicAuth) => void;
@@ -10,54 +10,60 @@ interface UserActions {
     setUser: (key: keyof typeof initialStates, value: any) => void;
     checkSession: () => void;
     isPremium: () => boolean;
-    validatePremium: (cb: () => void) => boolean;
+    validatePremium: (cb: () => void) => void;
 }
 
-export type UserStates = typeof initialStates;
 const initialStates = {
     isAuthenticated: false,
     user: null as User | null,
-}
+};
+
+export type UserStates = typeof initialStates;
+
 const useUser = create<UserStates & UserActions>()(
     (set, get) => ({
         ...initialStates,
         setUser: (key, value) => set({[key]: value}),
         isPremium: () => {
             const user = get().user;
+
             if (user) return user.type > 0;
             return false;
         },
         logout: () => {
             altogic.auth.signOut();
-            toast.success("Logged Out");
+            toast.success("Logged out.");
             useModal.setState({account: false});
             set(initialStates);
         },
+        login: response => {
+            set({user: response.user as any, isAuthenticated: true});
+        },
         checkSession: async () => {
             const currentSession = altogic.auth.getSession();
+
             if (currentSession) {
                 const dbUser = await altogic.auth.getUserFromDB();
-                set({user: dbUser.user as any, isAuthenticated: true})
+
+                altogic.auth.setSession(currentSession);
+                set({user: dbUser.user as any, isAuthenticated: true});
             } else {
                 if (!new URLSearchParams(window.location.search).get("access_token")) return;
+
                 const data = await altogic.auth.getAuthGrant();
                 if (!data.errors?.items.length) {
-                    set({user: data.user as any, isAuthenticated: true})
+                    set({user: data.user as any, isAuthenticated: true});
                 }
             }
-
         },
-        validatePremium: (cb) => {
+        validatePremium: callback => {
             if (get().isAuthenticated) {
                 if (!get().isPremium()) return useModal.getState().setVisible("premium")(true);
-                return cb();
+                return callback();
             } else {
                 return useModal.getState().setVisible("account")(true);
             }
         },
-        login: (response) => {
-            set({user: response.user, isAuthenticated: true});
-        }
-    })
-)
+    }));
+
 export default useUser;
