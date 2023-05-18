94d59b43c2b67efac9d2b25c6ed21222f40ab01b
behlole <behloleaqil@gmail.com>
2023-05-17 23:05:30 -0400

    implemented Modal files

--- src/containers/ModalController/index.tsx
+++ src/containers/ModalController/index.tsx
@@ -1,5 +1,13 @@
 import useModal from "../../store/useModal";
 import {shallow} from "zustand/shallow";
+import {ImportModal} from "../Modals/ImportModal";
+import {ClearModal} from "../Modals/ClearModal";
+import {DownloadModal} from "../Modals/DownloadModal";
+import {SettingsModal} from "../Modals/SettingsModal";
+import {CloudModal} from "../Modals/CloudModal";
+import {AccountModal} from "../Modals/AccountModal";
+import {PremiumModal} from "../Modals/PremiumModal";
+import {LoginModal} from "../Modals/LoginModal";
 
 export const ModalController = () => {
     const setVisible = useModal(state => state.setVisible);
@@ -29,7 +37,14 @@ export const ModalController = () => {
     )
     return (
         <>
+            <ImportModal opened={importModal} onClose={() => setVisible("import")(false)}/>
+            <ClearModal opened={clearModal} onClose={() => setVisible("clear")(false)}/>
+            <DownloadModal opened={downloadModal} onClose={() => setVisible("download")(false)}/>
+            <SettingsModal opened={settingsModal} onClose={() => setVisible("settings")(false)}/>
+            <CloudModal opened={cloudModal} onClose={() => setVisible("cloud")(false)}/>
+            <AccountModal opened={accountModal} onClose={() => setVisible("account")(false)}/>
+            <PremiumModal opened={premiumModal} onClose={() => setVisible("premium")(false)}/>
+            <LoginModal opened={loginModal} onClose={() => setVisible("login")(false)}/>
         </>
     )
-
 }
