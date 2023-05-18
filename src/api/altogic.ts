ca7967b726c354632c5d9bc59fb8fc9ae0993ce1
behlole <behloleaqil@gmail.com>
2023-05-17 23:00:41 -0400

    implemented ModalController

--- src/api/altogic.ts
+++ src/api/altogic.ts
@@ -4,7 +4,6 @@ import * as process from "process";
 let envUrl = process.env.NEXT_PUBLIC_ALTOGIC_ENV_URL as string;
 let clientKey = process.env.NEXT_PUBLIC_ALTOGIC_CLIENT_KEY as string;
 const altogic = createClient(envUrl, clientKey);
-
 export interface AltogicResponse<T> {
     data: T;
     errors: APIError | null;
